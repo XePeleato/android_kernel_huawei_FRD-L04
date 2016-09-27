7e38f1f3f1a1d72ac352d7fe3805f3ab22aa45b1
XePeleato <edu@error404software.com>
2016-09-27 22:54:12 +0200

    CI config test 4

--- travis.sh
+++ travis.sh
@@ -1,5 +1,5 @@
-LOCAL_DIR=`pwd`
-export PATH="$PATH:$(LOCAL_DIR)/prebuilts/bin/"
+
+export PATH="$PATH:$(pwd)/prebuilts/bin/"
 export CROSS_COMPILE="aarch64-linux-android-"
 mkdir -p out
 make ARCH=arm64 O=out merge_hi3650_defconfig
